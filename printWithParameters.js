9d5b27b02634cc30bab9ff427b66e132bcff9d47
Arnold Chan <arnoldthchan@gmail.com>
2017-07-03 15:03:09 +0000

    Added printing with editable parameters

---
+++ printWithParameters.js
@@ -0,0 +1,34 @@
+var https = require('https');
+function getAndPrintHTML(options){
+// This function should use a buffering technique
+// to append each chunk of data to a variable as it is received,
+//  and then console.log the data once all of the data has been received.
+// console.log(`Host: ${requestOptions.host}\nPath: ${requestOptions.path}`);
+var dataTotal = '';
+
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      dataTotal += data;
+
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(dataTotal);
+    });
+  })
+
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+getAndPrintHTML(requestOptions);

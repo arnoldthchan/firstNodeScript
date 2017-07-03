8b6bb07e1559b684886d157f5fcc77be84d9092e
Arnold Chan <arnoldthchan@gmail.com>
2017-07-03 14:27:25 +0000

    added first step

---
+++ test.js
@@ -0,0 +1,15 @@
+var https = require('https');
+function getAndPrintHTMLChunks(){
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+
+
+
+  console.log('if i had chunks of data\nit would be here');
+}
+
+getAndPrintHTMLChunks()
